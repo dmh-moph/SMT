6809de68881aae2e9b4c6f92ccd3d9f803c98224
Det Buaklee <dbuaklee@hotmail.com>
2014-10-13 16:57:18 +0700

    fix filtering

--- SMT/src/main/java/smt/config/WebConfig.java
+++ SMT/src/main/java/smt/config/WebConfig.java
@@ -1,16 +1,17 @@
 package smt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		  registry.addResourceHandler("/static/**")
-		    .addResourceLocations("classpath:/static/")
-		    .setCachePeriod(0);
+		    .addResourceLocations("classpath:/static/");
 	}
 	
 }
