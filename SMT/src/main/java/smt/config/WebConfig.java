b99d299525187a6720bd74c8ddf62047e246b24c
dbuaklee <dbuaklee@hotmail.com>
2014-10-17 02:01:50 +0700

    add @enablejparepository

--- SMT/src/main/java/smt/config/WebConfig.java
+++ SMT/src/main/java/smt/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -21,6 +22,7 @@ import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module.Feature;
 
 @Configuration
 @EnableWebMvc
+@EnableJpaRepositories
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
