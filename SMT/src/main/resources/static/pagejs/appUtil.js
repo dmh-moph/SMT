f769e44096320c17629e3f1bf73b9aacf2962fdf
Det Buaklee <dbuaklee@hotmail.com>
2014-10-21 17:59:10 +0700

    add helper for txt input

--- SMT/src/main/resources/static/pagejs/appUtil.js
+++ SMT/src/main/resources/static/pagejs/appUtil.js
@@ -10,6 +10,32 @@ Handlebars.registerHelper('formatNumber', function(number) {
 	return addCommas(number);
 });
 
+Handlebars.registerHelper('txtInput', function(size, label, field, defaultValue) {
+	
+	var aValue = "";
+	
+	if(defaultValue != null) {
+		aValue = defaultValue;
+	}
+	
+	var mdLabel = 2;
+	var mdTxt = 10;
+	if(size == "half") {
+		mdLabel = 4;
+		mdTxt = 8;
+	} 
+	
+	var s = "" +
+			"<div class='form-group'> \n" +
+			"	<label for='"+ field+"Txt' class='col-md-"+mdLabel+" control-label'>"+label+"</label> \n" +
+			"	<div class='col-md-"+mdTxt+"'> \n" +
+			"		<input type='text' class='form-control formTxt' id='"+ field+"Txt' data-field='"+field+"' value='"+aValue+"'></input> \n" +
+			"	</div> \n" +
+			"</div>"; 
+	
+	return new Handlebars.SafeString(s);
+});
+
 function __addCommas(nStr)
 {
 	if(nStr == null || isNaN(nStr)) {
